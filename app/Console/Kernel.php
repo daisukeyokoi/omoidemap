63fcf0100ff30e6b595f6dac85e21d7fbcf85979
matumoto yuki <abc123931@gmail.com>
2017-05-07 00:33:15 +0900

    cronの変更

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('inspire')
-                 ->hourly();
+                 ->everyMinute();
         // イベントの状態を変更
         $schedule->command('event:updatestate')
                  ->daily();
